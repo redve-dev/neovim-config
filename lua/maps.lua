aa600fc46da0c2f0efdafdcfcd5bdb78485b6a4a
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-06-21 02:23:04 +0200

    fix insert mode, tree support

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,15 +1,19 @@
 function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 map("n", "<F12>", ":set invhlsearch<CR>", {silent=true})
 map("n", "<F1>", "gg=G", {silent=true})
 map("n", "<F11>", ":set relativenumber!<CR>")
-map("i", "<F11>", ":set relativenumber!<CR>")
+map("i", "<F11>", "<Esc>:set relativenumber!<CR>")
 map("v", "<F11>", ":set relativenumber!<CR>")
 map("n", "<F2>", "A;<Esc>")
-map("i", "<F2>", "A;<Esc>")
+map("i", "<F2>", "<Esc>A;")
+map("n", "<F9>", ":lua require\"nvim-tree\".toggle(false, false)<CR>")
+map("i", "<F9>", "<Esc>:lua require\"nvim-tree\".toggle(false, true)<CR>")
+map("n", "<F10>", ":lua require\"nvim-tree\".toggle(false, true)<CR>")
+map("i", "<F10>", "<Esc>:lua require\"nvim-tree\".toggle(false, true)<CR>")
