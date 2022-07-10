413a49403efba83a580e700cd5538700a1ffa4c6
redve <106469071+redve-dev@users.noreply.github.com>
2022-07-10 19:42:40 +0200

    remove unsused line

--- lua/setup.lua
+++ lua/setup.lua
@@ -1,15 +1,6 @@
 require('nvim-autopairs').setup()
 vim.g.indent_blankline_char='┊'
---vim.cmd(
---[[
-----autocmd VimEnter * Neotree show
---]]
---)
-
 vim.cmd("autocmd BufEnter * set formatoptions-=cro")
 vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
-require('onedark').setup {
-    style = 'darker'
-}
-require('onedark').load()
+require('onedark').setup({style = 'darker' })
 vim.cmd[[colorscheme onedark]]
