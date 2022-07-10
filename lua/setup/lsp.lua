d66f430261cc066103f15f0af939db57a909c8c8
redve <106469071+redve-dev@users.noreply.github.com>
2022-07-10 22:10:58 +0200

    add window border

--- lua/setup/lsp.lua
+++ lua/setup/lsp.lua
@@ -71,8 +71,45 @@ cmp.setup.cmdline(':', {
 		{ name = 'cmdline' }
 	})
 })
-
 -- Setup lspconfig.
+
+vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=red guibg=#1f2335]]
+
+--local border = {
+      --{"🭽", "FloatBorder"},
+      --{"▔", "FloatBorder"},
+      --{"🭾", "FloatBorder"},
+      --{"▕", "FloatBorder"},
+      --{"🭿", "FloatBorder"},
+      --{"▁", "FloatBorder"},
+      --{"🭼", "FloatBorder"},
+      --{"▏", "FloatBorder"},
+--}
+local border = {
+	{ "╭", "FloatBorder"},
+	{"─", "FloatBorder"},
+	{"╮", "FloatBorder"},
+	{"│", "FloatBorder"},
+	{"╯", "FloatBorder"},
+	{"─", "FloatBorder"},
+	{"╰", "FloatBorder"},
+	{"│" , "FloatBorder"},
+}
+
+-- LSP settings (for overriding per client)
+local handlers =  {
+	["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
+	["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
+}
+-- To instead override globally
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+	opts = opts or {}
+	opts.border = opts.border or border
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 require'lspconfig'.clangd.setup {}
 require'lspconfig'.pyright.setup {}
