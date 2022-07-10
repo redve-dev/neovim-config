9bb9f50a98cc70090ef050b2afd6af0c518252c0
redve <106469071+redve-dev@users.noreply.github.com>
2022-07-10 19:35:50 +0200

    move config to setup dir

--- init.lua
+++ init.lua
@@ -1,10 +1,6 @@
 vim.cmd [[packadd packer.nvim]]
-require('impatient')
 require('plugins')
 require('setup')
 require('settings')
 require('maps')
-require('lsp')
-require('treesitter')
-require('nvimtree')
 require('highlight')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,47 +1,28 @@
-return require('packer').startup(
+return require("packer").startup(
 function()
-	use 'wbthomason/packer.nvim'
-	use 'windwp/nvim-autopairs'
-	use 'preservim/nerdcommenter'
-	use 'lukas-reineke/indent-blankline.nvim'
-	use 'rbgrouleff/bclose.vim'
-	use 'francoiscabrol/ranger.vim'
-	use "nvim-lua/plenary.nvim"
-	use 'nathom/filetype.nvim' 
-	use 'lewis6991/impatient.nvim'
-	use 'mg979/vim-visual-multi'
-	use {
-		"smjonas/inc-rename.nvim",
-		config = function()
-			require("inc_rename").setup()
-		end,
-	}
-	use {
-		'goolord/alpha-nvim',
-		config = function ()
-			require("alpha_conf")
-		end,
-	}
-	use 'kyazdani42/nvim-web-devicons'
-	use 'kyazdani42/nvim-tree.lua'
+	use ({"wbthomason/packer.nvim"})
+	use ({"windwp/nvim-autopairs"})
+	use ({"preservim/nerdcommenter"})
+	use ({"lukas-reineke/indent-blankline.nvim"})
+	use ({"mg979/vim-visual-multi"})
+	use ({"smjonas/inc-rename.nvim", config = require("setup/inc_rename") })
+	use ({ "goolord/alpha-nvim", config = require("setup/alpha") })
+	use ({"kyazdani42/nvim-web-devicons"})
+	use ({"kyazdani42/nvim-tree.lua", config = require("setup/nvimtree")})
 
 	--colors
-	use	'nvim-treesitter/nvim-treesitter'
-	use 'itchyny/lightline.vim'
-	use({
-		'projekt0n/github-nvim-theme',
-		config = function()
-			require('github-theme').setup({
-			})
-		end
-	})
+	use	({"nvim-treesitter/nvim-treesitter", config = require("setup/treesitter")})
+	use ({"itchyny/lightline.vim"})
+	use ({"navarasu/onedark.nvim"})
 
 	-- LSP
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
+	use "neovim/nvim-lspconfig"
+	use "hrsh7th/cmp-nvim-lsp"
+	use "hrsh7th/cmp-buffer"
+	use "hrsh7th/cmp-path"
+	use "hrsh7th/cmp-cmdline"
+	use ({"hrsh7th/nvim-cmp", config = require("setup/lsp")})
+	use ({"andweeb/presence.nvim", config = require("setup/presence")})
+	use ({ "folke/trouble.nvim", config = require("setup/trouble") })
 end
 )
